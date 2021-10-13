5f45a0844f89a63ce12640ac06717335ad5d30d1
ChekanKate <68741500+ChekanKate@users.noreply.github.com>
2021-10-13 16:53:46 +0300

    Add files via upload

---
+++ spring_course_mvc/src/main/java/com/chekan/spring/mvc/MyController.java
@@ -0,0 +1,49 @@
+package com.chekan.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/employee")
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model){
+        model.addAttribute("employee", new Employee());
+
+        return "ask-emp-details-view";
+    }
+
+//    @RequestMapping("showDetails")
+//    public String showEmployeeDetails(){
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(HttpServletRequest request, Model model){
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribite", empName);
+//        model.addAttribute("discription", " - udemi instructor");
+//        return "show-emp-details-view";
+//    }
+
+    @PostMapping("/showDetails")
+    public String showEmployeeDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "ask-emp-details-view";
+        }else {
+            return "show-emp-details-view";
+        }
+    }
+}
